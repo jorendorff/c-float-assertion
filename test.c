3121771163bd33e328701634c614a79a29910c48
Jason Orendorff <jorendorff@github.com>
2023-07-16 15:37:21 -0500

    renames

--- test.c
+++ test.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
 
-void trial(long long c, long long layer1_size, float *syn0, float *syn1, float g) {
-  float a = syn1[c];
-  float x = g * syn0[c];
+void trial(long long i, float *src, float *target, float g) {
+  float a = target[i];
+  float x = g * src[i];
   printf("%g %g %g\n", a, x, a + x);
-  syn1[c] += g * syn0[c];
-  assert(syn1[c] == a + x);
+  target[i] += g * src[i];
+  assert(target[i] == a + x);
 }
 
 int main() {
   float target[2] = { 0.0, -4.52271e-06 };
   float src[2] = { -0.000926437, -0.00102722 };
 
-  trial(1, 2, src, target, 0.01235);
+  trial(1, src, target, 0.01235);
   return 0;
 }
